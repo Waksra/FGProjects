b4fe42121424a0d633f25e9fa3521231a874cc63
Wiktor Ravndal <w.ravndal@gmail.com>
2020-09-08 14:59:03 +0200

    Camera Mover

---
+++ SHMUP/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [Range(0, 1)] public float smoothTime = 0.2f;
+    public Transform target;
+
+    private Vector2 _velocity;
+    
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        _transform.position = Vector2.SmoothDamp(transform.position, target.position, ref _velocity, smoothTime);
+    }
+}

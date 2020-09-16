f30998e85a01c5297a5de80260df366873d64272
Wiktor Ravndal <w.ravndal@gmail.com>
2020-09-16 16:47:44 +0200

    End of Day push

---
+++ SHMUP/Assets/Scripts/AsteroidPlacer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class AsteroidPlacer : MonoBehaviour
+{
+    [Range(0,1)]
+    public float threshold;
+    public float scale;
+    public float step;
+    public float range;
+    
+    public GameObject[] asteroidPrefabs;
+
+    private void Awake()
+    {
+        for (float x = -range; x <= range; x += step)
+        {
+            for (float y = -range; y <= range; y += step)
+            {
+                float perlinX = (x / range * scale) + range;
+                float perlinY = (y / range * scale) + range;
+
+                float value = Mathf.PerlinNoise(perlinX, perlinY);
+
+                if (value >= threshold)
+                {
+                    GameObject newAsteroid = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
+
+                    Instantiate(
+                        newAsteroid, 
+                        new Vector3(x,y), 
+                        Quaternion.Euler(0, 0, Random.Range(0,360)), 
+                        transform);   
+                }
+            }
+        }
+    }
+}
--- SHMUP/Assets/Scripts/BackgroundParallax.cs
+++ SHMUP/Assets/Scripts/BackgroundParallax.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class BackgroundParallax : MonoBehaviour
 {
-    [Range(0, 20)]
+    [Range(0, 1)]
     public float parallaxAmount = 5;
 
     private Transform _transform;
     private MeshRenderer _meshRenderer;
 
-    private int test;
+    private int _offsetID;
 
     private void Awake()
     {
         _transform = transform;
         _meshRenderer = GetComponent<MeshRenderer>();
-        test = Shader.PropertyToID("_MainOffset");
+        _offsetID = Shader.PropertyToID("_MainOffset");
     }
 
     private void LateUpdate()
@@ -28,6 +28,6 @@ public class BackgroundParallax : MonoBehaviour
         textureOffset.x = position.x / localScale.x * parallaxAmount;
         textureOffset.y = position.y / localScale.y * parallaxAmount;
 
-        _meshRenderer.material.SetVector(test, textureOffset);
+        _meshRenderer.material.SetVector(_offsetID, textureOffset);
     }
 }
